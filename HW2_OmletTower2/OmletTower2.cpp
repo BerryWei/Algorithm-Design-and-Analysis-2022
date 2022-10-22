476844804235b528af0841253ef1f63d7365a70d
Berry Wei <berrya90239@gmail.com>
2022-10-22 21:33:17 +0800

    HW2 Omlet Tower3 AC

--- HW2_OmletTower2/OmletTower2.cpp
+++ HW2_OmletTower2/OmletTower2.cpp
@@ -5,15 +5,24 @@
 
 using namespace std;
 
+long long int Mod(long long int a, long long int b, long long int M)
+{
+    long long int output = a + b ;
+    while( output >= M)
+        output = output - M;
+
+    return output;
+}
+
+
+
 long long int photos(long long int N, long long int K, long long int M)
 {
-    // delare DP multiarray DP[n][k][m] index 1 ~ n的個前綴合等於k 且用了 m 個 +1；
     // N 差分數對
     N = N+1;
-    K = K;
-    
+
     long long int **Matrix = new long long int* [K+1];
-    for(long long int i = 0; i < K+1 ; i++)
+    for(long long int i = 0; i <= K ; i++)
         Matrix[i] = new long long int [K+1];
     for(long long int i = 0; i<= K; i++)
         for(long long int j = 0; j<= K;j++)
@@ -27,59 +36,97 @@ long long int photos(long long int N, long long int K, long long int M)
         for(long long int j = 0; j<= K;j++)
             Save[i][j] = 0;
     //
+    long long int **UD = new long long int* [K+1];
+    for(long long int i = 0; i < K+1 ; i++)
+        UD[i] = new long long int [K+1];
+    for(long long int i = 0; i<= K; i++)
+        for(long long int j = 0; j<= K;j++)
+            UD[i][j] = 0;
+    
+    //
+    long long int **LR = new long long int* [K+1];
+    for(long long int i = 0; i < K+1 ; i++)
+        LR[i] = new long long int [K+1];
+    for(long long int i = 0; i<= K; i++)
+        for(long long int j = 0; j<= K;j++)
+            LR[i][j] = 0;
+
 
     // N = 2;
-    for(long long int i = 0; i <= K; i++)
+   for(long long int i = 0; i <= K; i++)
     {
         for(long long int j = 0;j <= i;j++)
         {
-            if(i == j)
-                Save[i][j] = 1;
-            else
+            if( j == 0)
+                Save[i][j] = i+1;
+            else if(j ==0 && i != 0)
                 Save[i][j] = 2;
+            else
+                Save[i][j] = 1;
         }
     }
+ 
+    
 
-    // N = 3
-    for(long long int n = 3; n<= N;n++)
+    
+
+   // Matrix[n][i][j] "用完" i 個 +1，"用完"j 個-1，有幾種圖形
+    for(int n = 3; n <= N; n++)
     {
-    long long int sum = 0;
-    for(long long int i = 0; i <= K; i++)
-    {
-        for(long long int j = 0; j<= i; j++)
+        for(int i = 0; i <= K; i++)
         {
-            sum += Save[i][j];
-            Matrix[i][j] = sum;
+            long long int temp = 0;
+            for(int j = 0; j<= i; j++)
+            {
+               temp += Save[i][j];
+               //temp = Mod(temp, Save[i][j], M);
+               LR[i][j] = temp;
+            }
         }
-    }
-    // Copy Matrix to Save
-    for(long long int i = 0; i <= K; i++)
-    {
-        for(long long int j = 0; j<= i; j++)
+
+        for(int j = 0; j <= K; j++)
         {
-            Save[i][j] = Matrix[i][j];
+            long long int temp = 0;
+            for(int i = j; i<= K; i++)
+            {
+                temp += Save[i][j];
+                //temp = Mod(temp, Save[i][j], M);
+                UD[i][j] = temp;
+            }
         }
-    }
 
-    // PRINT
-/*
-for(int i = 0; i <= K; i++)
-{
-    for(int j = 0; j<= i; j++)
-    {
-        cout << Matrix[i][j] << " ";
-    }
-    cout << endl;
-}
-*/
+        for(int i = 0; i <= K; i++)
+        {
+            for(int j = 0; j<= i; j++)
+            {
+                //Matrix[i][j] = LR[i][j] + UD[i][j] - Save[i][j];
+                //Matrix[i][j] = Mod(Matrix[i][j], LR[i][j], M);
+                //Matrix[i][j] = Mod(Matrix[i][j], UD[i][j], M);
+                //Matrix[i][j] = Mod(Matrix[i][j], -1*Save[i][j], M);
+                Matrix[i][j] = (LR[i][j] + UD[i][j] - Save[i][j]) % M;
+
+            }
+        }
+
+        // Matrix -> Save
+        for(int i=0; i<= K; i++)
+        {
+            for(int j =0; j<=K; j++)
+                Save[i][j] = Matrix[i][j];
+        }
+        
     }
     
 
-    long long int output = Matrix[K][K];
-    output = output % M;
     
-    return output;
     
+
+    long long int output = 0;
+    for(int i = 0; i <= K; i++)
+        output = Mod(output, Matrix[i][i], M);
+
+
+    return output;    
 }
 
 int main()
